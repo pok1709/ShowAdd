4742adf7996bee6a14b39b48f92d868f8d9cc7db
pok1709 <pok1709@gmail.com>
2017-09-09 14:19:54 +0700

    Done

--- Show Add/ViewController.swift
+++ Show Add/ViewController.swift
@@ -10,6 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var xString = ""
+    var yString = ""
+    
+    @IBOutlet weak var xOutlet: UITextField!
+    
+    @IBOutlet weak var yOutlet: UITextField!
+    
+    @IBOutlet weak var answerOutlet: UILabel!
+    
+    
+    
+    @IBAction func calButton(_ sender: Any) {
+        xString = xOutlet.text!
+        yString = yOutlet.text!
+        
+        print("X Value ==> \(xString)")
+        print("Y Value ==> \(yString)")
+        
+        let xAInt = Int(xString)
+        let yAInt = Int(yString)
+        
+        let answerAInt = xAInt! + yAInt!
+        
+        let answerString = String(answerAInt)
+        
+        answerOutlet.text = answerString
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
